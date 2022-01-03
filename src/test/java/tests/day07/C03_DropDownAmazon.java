ff611b2cec2ffd40b0021363915028e44802ae89
Mustafa Gecer <h.musti.27@gmail.com>
2022-01-03 18:10:42 +0100

    day08

--- src/test/java/tests/day07/C03_DropDownAmazon.java
+++ src/test/java/tests/day07/C03_DropDownAmazon.java
@@ -1,5 +1,68 @@
 package tests.day07;
 
-public class C03_DropDownAmazon {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
+public class C03_DropDownAmazon {
+    // Bir class oluşturun: D10_DropDownAmazon
+//● https://www.amazon.com/ adresine gidin.
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com/");
+
+
+
+
+    }
+    //- Test 1
+//Arama kutusunun yanindaki kategori menusundeki kategori sayisinin
+//45 oldugunu test edin
+    @Test
+    public void  secenekSayisi(){
+        WebElement dropDown=driver.findElement(By.id("searchDropdownBox"));
+        Select select=new Select(dropDown);
+        System.out.println("Secenek sayisi: "+select.getOptions().size());
+    }
+
+    //-Test 2
+//1. Kategori menusunden Books secenegini secin
+//2. Arama kutusuna Java yazin ve aratin
+//3. Bulunan sonuc sayisini yazdirin
+//4. Sonucun Java kelimesini icerdigini test edin
+    @Test
+    public void searchBox() throws InterruptedException {
+        WebElement dropDown=driver.findElement(By.id("searchDropdownBox"));
+        Select select=new Select(dropDown);
+        select.selectByVisibleText("Books");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java"+ Keys.ENTER);
+        Thread.sleep(3000);
+        WebElement sonuclar=driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        System.out.println(sonuclar.getText());
+
+        Assert.assertTrue(sonuclar.getText().contains("Java"));
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.close();
+    }
 }
