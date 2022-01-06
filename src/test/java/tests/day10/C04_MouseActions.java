2b40029c1aa356145d31525d2bd63097d48edaea
Mustafa Gecer <h.musti.27@gmail.com>
2022-01-06 15:56:57 +0100

    day11

--- src/test/java/tests/day10/C04_MouseActions.java
+++ src/test/java/tests/day10/C04_MouseActions.java
@@ -1,4 +1,34 @@
 package tests.day10;
 
-public class C04_MouseActions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C04_MouseActions extends TestBase {
+
+    @Test
+    public void test() throws InterruptedException {
+        //Yeni bir class olusturalim: D14_MouseActions2
+        //1- https://demoqa.com/droppable adresine gidelim
+        driver.get("https://demoqa.com/droppable");
+        //2- “Drag me” butonunu tutup “Drop here” kutusunun ustune birakalim
+
+
+        WebElement draggableWebElementi=driver.findElement(By.id("draggable"));
+        WebElement droppableWebElementi=driver.findElement(By.id("droppable"));
+
+        Actions actions=new Actions(driver);
+        actions.dragAndDrop(draggableWebElementi,droppableWebElementi).perform();
+        Thread.sleep(2000);
+
+        //3- “Drop here” yazisi yerine “Dropped!” oldugunu test edin
+        System.out.println(droppableWebElementi.getText());
+        Assert.assertEquals(droppableWebElementi.getText(),"Dropped!","Drop yazisi Dropped! yazisina donusmedi");
+
+
+
+    }
 }
